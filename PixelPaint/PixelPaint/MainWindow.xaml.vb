266d25dcff61311bdfb498a96cf50fa5afda3721
zacwi057 <zacwi057@learner.dvusd.org>
2024-12-05 14:10:57 -0700

    Pixel paint update 2

--- PixelPaint/PixelPaint/MainWindow.xaml.vb
+++ PixelPaint/PixelPaint/MainWindow.xaml.vb
@@ -1,3 +1,5 @@
 ﻿Class MainWindow
-
+    Private Sub Fill1_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Fill1.MouseDown, Fill2.MouseDown, Fill3.MouseDown, Fill3.MouseDown, Fill4.MouseDown, Fill5.MouseDown, Fill6.MouseDown, Fill7.MouseDown, Fill8.MouseDown
+        Brush1.Fill = sender.fill
+    End Sub
 End Class
