fb6e705385f2e0f0438de15492d19ae0b12721e3
zacwi057 <zacwi057@learner.dvusd.org>
2024-12-06 12:35:06 -0700

    PixelPaint #3

--- PixelPaint/PixelPaint/MainWindow.xaml.vb
+++ PixelPaint/PixelPaint/MainWindow.xaml.vb
@@ -2,4 +2,10 @@
     Private Sub Fill1_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles Fill1.MouseDown, Fill2.MouseDown, Fill3.MouseDown, Fill3.MouseDown, Fill4.MouseDown, Fill5.MouseDown, Fill6.MouseDown, Fill7.MouseDown, Fill8.MouseDown
         Brush1.Fill = sender.fill
     End Sub
+
+    Private Sub p00_MouseDown(sender As Object, e As MouseButtonEventArgs) Handles p00.MouseDown, p01.MouseDown, p02.MouseDown, p03.MouseDown, p04.MouseDown, p05.MouseDown, p06.MouseDown, p07.MouseDown, p08.MouseDown, p09.MouseDown, p10.MouseDown, p11.MouseDown, p12.MouseDown, p13.MouseDown, p14.MouseDown, p15.MouseDown, p16.MouseDown, p17.MouseDown, p18.MouseDown, p19.MouseDown, p20.MouseDown, p21.MouseDown, p22.MouseDown, p23.MouseDown, p24.MouseDown, p25.MouseDown, p26.MouseDown, p27.MouseDown, p28.MouseDown, p29.MouseDown, p30.MouseDown, p31.MouseDown, p32.MouseDown, p33.MouseDown, p34.MouseDown, p35.MouseDown, p36.MouseDown, p37.MouseDown, p38.MouseDown, p39.MouseDown, p40.MouseDown, p41.MouseDown, p42.MouseDown, p43.MouseDown, p44.MouseDown, p45.MouseDown, p46.MouseDown, p47.MouseDown, p48.MouseDown, p49.MouseDown, p50.MouseDown, p51.MouseDown, p52.MouseDown, p53.MouseDown, p54.MouseDown, p55.MouseDown, p56.MouseDown, p57.MouseDown, p58.MouseDown, p59.MouseDown, p60.MouseDown, p61.MouseDown, p62.MouseDown, p63.MouseDown, p64.MouseDown, p65.MouseDown, p66.MouseDown, p67.MouseDown, p68.MouseDown, p69.MouseDown, p70.MouseDown, p71.MouseDown, p72.MouseDown, p73.MouseDown, p74.MouseDown, p75.MouseDown, p76.MouseDown, p77.MouseDown, p78.MouseDown, p79.MouseDown, p80.MouseDown, p81.MouseDown, p82.MouseDown, p83.MouseDown, p84.MouseDown, p85.MouseDown, p86.MouseDown, p87.MouseDown, p88.MouseDown, p89.MouseDown, p90.MouseDown, p91.MouseDown, p92.MouseDown, p93.MouseDown, p94.MouseDown, p95.MouseDown, p96.MouseDown, p97.MouseDown, p98.MouseDown, p99.MouseDown
+
+
+        sender.fill = Brush1.Fill
+    End Sub
 End Class
