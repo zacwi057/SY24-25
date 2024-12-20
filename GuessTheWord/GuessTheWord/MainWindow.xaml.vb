a42d5634ed79ea58d745d49db78510b2ef811f4c
zacwi057 <zacwi057@learner.dvusd.org>
2024-12-20 10:44:53 -0700

    Guess The Word

---
+++ GuessTheWord/GuessTheWord/MainWindow.xaml.vb
@@ -0,0 +1,124 @@
+﻿Class MainWindow
+    Private Sub Button_Click(sender As Object, e As RoutedEventArgs)
+        A.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub Button_Click_1(sender As Object, e As RoutedEventArgs)
+        B.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub Button_Click_2(sender As Object, e As RoutedEventArgs)
+        C.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub Button_Click_3(sender As Object, e As RoutedEventArgs)
+        D.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub Button_Click_4(sender As Object, e As RoutedEventArgs)
+        Panel3.Visibility = Visibility.Visible
+        Panel4.Visibility = Visibility.Visible
+
+    End Sub
+
+    Private Sub Button_Click_5(sender As Object, e As RoutedEventArgs)
+        F.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub Button_Click_6(sender As Object, e As RoutedEventArgs)
+        G.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub Button_Click_7(sender As Object, e As RoutedEventArgs)
+        H.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub Button_Click_8(sender As Object, e As RoutedEventArgs)
+        I.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub J_Click(sender As Object, e As RoutedEventArgs) Handles J.Click
+        J.Background = New SolidColorBrush(Colors.Red)
+    End Sub
+
+    Private Sub K_Click(sender As Object, e As RoutedEventArgs) Handles K.Click
+        K.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub L_Click(sender As Object, e As RoutedEventArgs) Handles L.Click
+        L.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub M_Click(sender As Object, e As RoutedEventArgs) Handles M.Click
+        M.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub N_Click(sender As Object, e As RoutedEventArgs) Handles N.Click
+        N.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub O_Click(sender As Object, e As RoutedEventArgs) Handles O.Click
+        O.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub P_Click(sender As Object, e As RoutedEventArgs) Handles P.Click
+        P.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub Q_Click(sender As Object, e As RoutedEventArgs) Handles Q.Click
+        Q.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub R_Click(sender As Object, e As RoutedEventArgs) Handles R.Click
+        Panel2.Visibility = Visibility.Visible
+
+    End Sub
+
+    Private Sub S_Click(sender As Object, e As RoutedEventArgs) Handles S.Click
+        Panel5.Visibility = Visibility.Visible
+
+
+    End Sub
+
+    Private Sub T_Click(sender As Object, e As RoutedEventArgs) Handles T.Click
+        Panel1.Visibility = Visibility.Visible
+
+    End Sub
+
+    Private Sub U_Click(sender As Object, e As RoutedEventArgs) Handles U.Click
+        U.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub V_Click(sender As Object, e As RoutedEventArgs) Handles V.Click
+        V.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub W_Click(sender As Object, e As RoutedEventArgs) Handles W.Click
+        W.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub X_Click(sender As Object, e As RoutedEventArgs) Handles X.Click
+        X.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub Y_Click(sender As Object, e As RoutedEventArgs) Handles Y.Click
+        Y.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+
+    Private Sub Z_Click(sender As Object, e As RoutedEventArgs) Handles Z.Click
+        Z.Background = New SolidColorBrush(Colors.Red)
+
+    End Sub
+End Class
